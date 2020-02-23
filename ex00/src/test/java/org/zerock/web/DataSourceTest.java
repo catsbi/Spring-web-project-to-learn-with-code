d533de2890270d45307d7397e8dc7b801f7e62dc
kwon1993 <wdmono@naver.com>
2020-02-23 18:28:08 +0900

    171p

--- ex00/src/test/java/org/zerock/web/DataSourceTest.java
+++ ex00/src/test/java/org/zerock/web/DataSourceTest.java
@@ -5,13 +5,10 @@ import java.sql.Connection;
 import javax.inject.Inject;
 import javax.sql.DataSource;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
@@ -22,7 +19,6 @@ public class DataSourceTest {
 	
 	@Test
 	public void testConection() throws Exception{
-		
 		try(Connection con = ds.getConnection()){
 			System.out.println(con);
 		}catch(Exception e) {
